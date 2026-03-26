df23dcb7ac8277363d9e1290c07a20a832375a14
TheBigRatkk <krassotar@gmail.com>
2026-03-26 08:55:45 -0300

    Commit n2 260326

---
+++ RafaelKrassota_1173.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class RafaelKrassota_1173 {
+
+    public static Scanner TECLADO = new Scanner(System.in);
+
+    public static int lerInt() {
+        return TECLADO.nextInt();
+    }
+
+    public static void preencherVetor(int[] vetor) {
+        for (int i = 1; i < vetor.length; i++) {
+            //vetor
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+
+    
+}
