5b66a35d7d236758d835f62df56408fa6e57c5a5
TheBigRatkk <krassotar@gmail.com>
2026-09-18 09:33:38 -0300

    commit n21 180926

--- RafaelKrassota_1010.java
+++ RafaelKrassota_1010.java
@@ -1,5 +1,23 @@
 import java.util.Scanner;
 
 public class RafaelKrassota_1010 {
-    
+
+    final static Scanner LER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int codego, codego2, qtd, qtd2;
+        double valor, valor2, qtdFinal;
+
+        codego = LER.nextInt();
+        qtd = LER.nextInt();
+        valor = LER.nextDouble();
+
+        codego2 = LER.nextInt();
+        qtd2 = LER.nextInt();
+        valor2 = LER.nextDouble();
+
+        qtdFinal = (valor * qtd) + (valor2 * qtd2);
+
+        System.out.printf("VALOR A PAGAR: R$ %.2f\n", qtdFinal);
+    }
 }
